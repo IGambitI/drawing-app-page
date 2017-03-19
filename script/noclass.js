1712a639f3f7fc01b121309fcf11b7eda16f11eb
Filip <Filip>
2017-03-19 20:59:42 +0100

    jak ja nie lubię gita

--- script/noclass.js
+++ script/noclass.js
@@ -1,18 +1,71 @@
-$(function(){
-  var w = $(window).width();
-  var h = $(window).height();
-  var slides = $('.slide1 > div');
-  $('.container').css({ height: (h-60) + 'px' });
-  $('.slide1').css({ width: slides.length + '00%' });
-  slides.css({ width: w + 'px' });
+var wp = (function(){
 
-  var pos = 0;
+	var $sliderBox = $("#slider");
 
-  $('.right').click(function(){
-  pos--;
-  $('.slide1').animate({ left: (pos * w) + 'px' });
-});
-$('.left').click(function(){
-  pos++;
-  $('.slide1').animate({ left: (pos * w) + 'px' });
-});
+	var Slider = function($slider)
+	{
+		var currentSlide = 0;
+		var records = null;
+		var $slides = $slider.find("div").filter("#slides");
+
+		var slides = []
+		var html = "";
+
+	
+		var handler = function(e)
+		{
+			var index = $(e.target).index();
+
+			if (index == 2)
+			{
+				currentSlide++;
+			}
+			else
+			{
+				currentSlide--;
+			}
+
+			if (currentSlide > 2)
+			{
+				currentSlide = 0;
+			}
+			if (currentSlide < 0)
+			{
+				currentSlide = 2;
+			}
+
+			$slides.css("margin-left", -currentSlide*1100);
+
+		}
+		var loadSlides = function()
+		{
+				$.ajax({
+					url: "/slides",
+					method: "POST",
+					success: function(data){
+						for (var i = 0; i < data.length; i++)
+						{
+							html += "<div class='dane'></div>";
+							html += "<img src=\""+data[i].image+"\"/>";
+						}
+						$slides.width(data.length*1100);
+						$slides.html(html);
+					}
+				});
+		}
+		loadSlides();
+
+		$slider.find("div").filter(".controll").on("click", handler);
+
+
+		this.next 		= next;
+		this.prev 		= prev;
+		this.current 	= currentSlide;
+	}
+
+	return {
+		slider: new Slider($sliderBox)
+	}
+
+
+})();
